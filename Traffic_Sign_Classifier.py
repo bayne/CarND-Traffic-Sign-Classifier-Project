bef9cc64ff5a025c17acc7d7230da2a647cd21bf
Brian Payne <bpayne@uci.edu>
2017-03-17 15:03:51 -0700

    Update Traffic_Sign_Classifier.py
    Added dropout
    increased epochs
    Test Accuracy = 0.914

--- Traffic_Sign_Classifier.py
+++ Traffic_Sign_Classifier.py
@@ -1,18 +1,4 @@
 import pickle
-
-import tensorflow as tf
-from tensorflow.contrib.layers import flatten
-
-def LeNet(x):
-    # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
-    mu = 0
-    sigma = 0.1
-
-    # SOLUTION: Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
-    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), mean=mu, stddev=sigma))
-    conv1_b = tf.Variable(tf.zeros(6))
-    conv1 = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
-import pickle
 import numpy as np
 
 import tensorflow as tf
@@ -119,7 +105,7 @@ def LeNet(x, dropout_prob):
 
     # SOLUTION: Activation.
     fc1 = tf.nn.relu(fc1)
-#    fc1 = tf.nn.dropout(fc1, dropout_prob)
+    fc1 = tf.nn.dropout(fc1, dropout_prob)
 
     # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
     fc2_W = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=mu, stddev=sigma))
@@ -128,7 +114,7 @@ def LeNet(x, dropout_prob):
 
     # SOLUTION: Activation.
     fc2 = tf.nn.relu(fc2)
-#    fc2 = tf.nn.dropout(fc2, dropout_prob)
+    fc2 = tf.nn.dropout(fc2, dropout_prob)
 
     # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 10.
     fc3_W = tf.Variable(tf.truncated_normal(shape=(84, n_classes), mean=mu, stddev=sigma))
@@ -155,12 +141,10 @@ n_classes = len(set(test["labels"]))
 width, height = len(test["features"][0]), len(test["features"][0][0])
 image_shape = (width, height)
 
-print(image_shape)
-
-EPOCHS = 10
-BATCH_SIZE = 256
+EPOCHS = 20
+BATCH_SIZE = 128
 LEARNING_RATE = 0.001
-DROPOUT = 0.60
+DROPOUT = 0.90
 
 features_placeholder = tf.placeholder(tf.float32, (None, height, width, None), name='features_placeholder')
 features = tf.image.rgb_to_grayscale(features_placeholder)
@@ -209,135 +193,6 @@ with tf.Session() as sess:
             end = offset + BATCH_SIZE
             batch_x, batch_y = X_train[offset:end], y_train[offset:end]
             sess.run(training_operation, feed_dict={features_placeholder: batch_x, logits_placeholder: batch_y, dropout_prob:DROPOUT})
-
-        # TODO are the labels formatted correctly?
-        validation_accuracy = evaluate(valid["features"], valid["labels"])
-        print("EPOCH {} ...".format(i + 1))
-        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
-        print()
-
-    saver.save(sess, './lenet')
-    print("Model saved")
-
-with tf.Session() as sess:
-    saver.restore(sess, tf.train.latest_checkpoint('.'))
-
-    test_accuracy = evaluate(test["features"], test["labels"])
-    print("Test Accuracy = {:.3f}".format(test_accuracy))
-
-    # SOLUTION: Activation.
-    conv1 = tf.nn.relu(conv1)
-
-    # SOLUTION: Pooling. Input = 28x28x6. Output = 14x14x6.
-    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
-    # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
-    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=mu, stddev=sigma))
-    conv2_b = tf.Variable(tf.zeros(16))
-    conv2 = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
-
-    # SOLUTION: Activation.
-    conv2 = tf.nn.relu(conv2)
-
-    # SOLUTION: Pooling. Input = 10x10x16. Output = 5x5x16.
-    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
-    # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
-    fc0 = flatten(conv2)
-
-    # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
-    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=mu, stddev=sigma))
-    fc1_b = tf.Variable(tf.zeros(120))
-    fc1 = tf.matmul(fc0, fc1_W) + fc1_b
-
-    # SOLUTION: Activation.
-    fc1 = tf.nn.relu(fc1)
-
-    # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
-    fc2_W = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=mu, stddev=sigma))
-    fc2_b = tf.Variable(tf.zeros(84))
-    fc2 = tf.matmul(fc1, fc2_W) + fc2_b
-
-    # SOLUTION: Activation.
-    fc2 = tf.nn.relu(fc2)
-
-    # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 10.
-    fc3_W = tf.Variable(tf.truncated_normal(shape=(84, n_classes), mean=mu, stddev=sigma))
-    fc3_b = tf.Variable(tf.zeros(n_classes))
-    logits = tf.matmul(fc2, fc3_W) + fc3_b
-
-    return logits
-
-training_file = 'train.p'
-validation_file = 'valid.p'
-testing_file = 'test.p'
-
-with open(training_file, mode='rb') as f:
-    train = pickle.load(f)
-with open(validation_file, mode='rb') as f:
-    valid = pickle.load(f)
-with open(testing_file, mode='rb') as f:
-    test = pickle.load(f)
-
-n_train = len(train["features"])
-n_test = len(test["features"])
-n_classes = len(set(test["labels"]))
-
-width, height = len(test["features"][0]), len(test["features"][0][0])
-image_shape = (width, height)
-
-print(image_shape)
-
-EPOCHS = 40
-BATCH_SIZE = 2048
-LEARNING_RATE = 0.001
-
-features_placeholder = tf.placeholder(tf.float32, (None, height, width, None), name='features_placeholder')
-features = tf.image.rgb_to_grayscale(features_placeholder)
-
-# why int32? maybe because they are unscaled logits, pixel values are int32
-logits_placeholder = tf.placeholder(tf.int32, (None), name='logits_placeholder')
-one_hot = tf.one_hot(logits_placeholder, n_classes)
-# logits = LeNet(features_placeholder)
-logits = LeNet(features)
-
-correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot, 1))
-accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-saver = tf.train.Saver()
-
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=one_hot)
-loss_operation = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
-training_operation = optimizer.minimize(loss_operation)
-
-
-def evaluate(X_data, y_data):
-    num_examples = len(X_data)
-    total_accuracy = 0
-    sess = tf.get_default_session()
-    for offset in range(0, num_examples, BATCH_SIZE):
-        batch_x, batch_y = X_data[offset:offset + BATCH_SIZE], y_data[offset:offset + BATCH_SIZE]
-        accuracy = sess.run(accuracy_operation, feed_dict={features_placeholder: batch_x, logits_placeholder: batch_y})
-        total_accuracy += (accuracy * len(batch_x))
-    return total_accuracy / num_examples
-
-
-# ## Train the Model
-
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    # num_examples = tra
-
-    print("Training...")
-    print()
-    for i in range(EPOCHS):
-        # TODO Shuffle?
-#        X_train, y_train = shuffle(train["features"], train["labels"])
-        for offset in range(0, n_train, BATCH_SIZE):
-            end = offset + BATCH_SIZE
-            batch_x, batch_y = train["features"][offset:end], train["labels"][offset:end]
-            sess.run(training_operation, feed_dict={features_placeholder: batch_x, logits_placeholder: batch_y})
 
         # TODO are the labels formatted correctly?
         validation_accuracy = evaluate(valid["features"], valid["labels"])
